44a632f32075d686dd161b05930b008326d33028
Sejin Cho <Sejin991009@gmail.com>
2025-06-17 18:24:41 +0900

    init

---
+++ src/main/java/com/sjp/flow/FlowService.java
@@ -0,0 +1,5 @@
+package com.sjp.flow;
+
+public class FlowService {
+    
+}
---
+++ src/test/java/com/flow/sjp/SjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.sjp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
